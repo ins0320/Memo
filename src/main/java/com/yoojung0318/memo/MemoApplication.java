146b43b1cc0f63a08f1ebf402cc200d5fa3d3296
gyagoo <ish96116@gmail.com>
2022-08-03 20:34:35 +0900

    [김유정] - 최초파일 생성

---
+++ src/main/java/com/yoojung0318/memo/MemoApplication.java
@@ -0,0 +1,16 @@
+package com.yoojung0318.memo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+@SpringBootApplication
+public class MemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemoApplication.class, args);
+	}
+
+}
